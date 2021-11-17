cccb16b8b43e8751b1fe4f053a85fac582c8e0bb
Thomas <tn2388@columbia.edu>
2021-11-17 01:50:57 -0500

    Updated to include random mole movement and event listener. However, you can click a hole and score will be updated. Need to fix

--- game.js
+++ game.js
@@ -1,12 +1,17 @@
-//Instance variables for timer 
+//Instance variables 
 const minutes = 1;
-let totalTime = minutes * 60;  
+let totalTime = minutes * 60; 
+let score = 0; 
+let clickedBox = null; 
 
-//Fetch HTML elements for update
+
+//Fetch HTML elements
 const timerElement = document.getElementById('timer'); 
+const scoreElement = document.getElementById('scoreboard'); 
+const boxElement = document.querySelectorAll('.box');
 
+//Countdown timer
 setInterval(countdownTimer, 1000);
-
 function countdownTimer() { 
     while (totalTime >= 0) {
         let seconds = totalTime; 
@@ -16,3 +21,30 @@ function countdownTimer() {
     }
 }
 
+//Update scoreboard on each mole whack
+function updateScore() {
+    score = score + 10; 
+}
+
+//Randomize mole appearances
+function getRandomMole() {
+    boxElement.forEach(box => {box.classList.remove('mole')});
+    let randomMole = boxElement[Math.floor(Math.random() * 9)]; 
+    randomMole.classList.add('mole');
+    clickedBox = randomMole.id
+}
+
+function moveMole() {
+    let movingMoles = setInterval(getRandomMole, 750);
+}
+moveMole(); 
+
+boxElement.forEach(box => {
+    box.addEventListener('click', ()=> {
+        if (box.id = clickedBox) {
+            updateScore(); 
+            scoreElement.innerHTML = score; 
+            clickedBox = null;
+        }
+    })
+})
