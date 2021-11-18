12f9f27cc8bf734660a9782e22f42de50773ed46
Thomas <tn2388@columbia.edu>
2021-11-18 00:09:08 -0500

    Updated refresh error where score was not updated

--- game.js
+++ game.js
@@ -1,17 +1,16 @@
-//Instance variables 
-const minutes = 1;
-let totalTime = minutes * 60; ; 
-let score = 0; 
-let moleBoxId = null; 
-
-var moleImg = document.createElement("moleImage"); 
-moleImg.src = "mole.png"; 
-
 //Fetch HTML elements
 const timerElement = document.getElementById('timer'); 
 const scoreElement = document.getElementById('scoreboard'); 
-const boxElement = document.querySelectorAll('.box');
+let boxElement = document.querySelectorAll('.box');
 const resetButtonElement = document.getElementById('reset-button'); 
+const gridElement = document.getElementById('grid');
+
+//Instance variables 
+const minutes = 0.1;
+let totalTime = minutes * 60; ; 
+let score = 0; 
+scoreElement.innerHTML = score;
+let moleBoxId = null; 
 
 //Countdown timer
 function countdownTimer() { 
@@ -19,13 +18,12 @@ function countdownTimer() {
     if (seconds == 0){
         clearInterval(gameTimer); 
         clearInterval(playMole);
-        alert('Game over! Thanks for playing!'); 
+        gridElement.innerHTML = '<div class="game-over">Game Over!</div>'; 
     }
     else{
         totalTime--; 
         timerElement.innerHTML = `${seconds}`; 
     }
-        
 }
 
 //Update scoreboard on each mole whack
@@ -48,7 +46,6 @@ function getRandomMole() {
     return moleBoxId; 
 }
 
-
 //Event listener for click to simulate mole whacking 
 boxElement.forEach(box => {
     box.addEventListener('click', () => {
@@ -57,6 +54,7 @@ boxElement.forEach(box => {
             updateScore(); 
             scoreElement.innerHTML = score; 
             moleBoxId = null;
+            console.log('I was clicked!'); 
         }
     })
 })
@@ -64,6 +62,20 @@ boxElement.forEach(box => {
 //Reset game: reset timer, score
 //Stops the current playMole, and starts a new randomizer
 function resetGame() {
+    //Regrab new boxes, add event listeners to each one 
+    gridElement.innerHTML = '<div class="box" id="1"><img src="hole.png"></div><div class="box" id="2"><img src="hole.png"></div><div class="box" id="3"><img src="hole.png"></div><div class="box" id="4"><img src="hole.png"></div><div class="box" id="5"><img src="hole.png"></div><div class="box" id="6"><img src="hole.png"></div><div class="box" id="7"><img src="hole.png"></div><div class="box" id="8"><img src="hole.png"></div><div class="box" id="9"><img src="hole.png"></div>'
+    boxElement = document.querySelectorAll('.box');
+    boxElement.forEach(box => {
+        box.addEventListener('click', () => {
+            if (box.classList.contains('mole')) {
+                document.getElementById(moleBoxId).innerHTML = '<img src="hole.png"/>';
+                updateScore(); 
+                scoreElement.innerHTML = score; 
+                moleBoxId = null;
+                console.log('I was clicked!'); 
+            }
+        })
+    })
     clearInterval(gameTimer);
     clearInterval(playMole);
     totalTime = 60;
@@ -73,6 +85,7 @@ function resetGame() {
     playMole = setInterval(getRandomMole, 750);
 }
 
+//Event listener for reset button, invokes resetGame()
 resetButtonElement.addEventListener('click', resetGame);
 
 let gameTimer = setInterval(countdownTimer, 1000);
